a2782365de51aa4a9b25935016cc6fed3ab07ecf
KaungMinKhant <kaungminkhant.edka@gmail.com>
2025-06-15 11:01:41 +0630

    permission

--- models/submodule.js
+++ models/submodule.js
@@ -1,30 +1,29 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class SubModule extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Role, {
-        foreignKey: "role_id",
-        as: "role",
+      // define association here
+      SubModule.belongsTo(models.Module, {
+        foreignKey: "module_id",
+        as: "module",
       });
     }
   }
-  User.init(
+  SubModule.init(
     {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      email: DataTypes.STRING,
-      role_id: DataTypes.INT,
+      sub_module_name: DataTypes.STRING,
+      module_id: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "SubModule",
     }
   );
-  return User;
+  return SubModule;
 };
